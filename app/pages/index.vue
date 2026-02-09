c3583d9e3177e62194d15134da59a3201391d68b
nicedson23 <nicedson23@gmail.com>
2026-02-09 22:23:30 +0700

    hide index page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,12 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import logoFull from "~/assets/images/logo-full.png";
+</script>
 
 <template>
   <NuxtLayout name="client-layouts">
-    <ProductDetail />
-    <ProductOption />
-    <Recipient />
-    <ProductSale />
-    <Action />
-    <ProductReview />
+    <img :src="logoFull" alt="logo" style="margin-block: auto" />
   </NuxtLayout>
 </template>
