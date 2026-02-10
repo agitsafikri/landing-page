df97df2bd21afdd37844640ff9291d006d8adb91
Nicholas Edson <nicedson23@gmail.com>
2026-02-10 09:54:36 +0700

    add embededPurchaseScript on success page

--- app/pages/[slug]/success.vue
+++ app/pages/[slug]/success.vue
@@ -1,8 +1,18 @@
 <script setup lang="ts">
 const pesananStore = usePesananStore();
+const produkStore = useProdukStore();
 
 onMounted(() => {
   if (!pesananStore.isSubmitted) navigateTo("/");
+
+  useHead({
+    script: [
+      {
+        innerHTML: produkStore.item.embededPurchaseScript,
+        type: "text/javascript",
+      },
+    ],
+  });
 });
 </script>
 
